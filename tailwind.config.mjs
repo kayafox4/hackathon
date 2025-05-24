14769a30fe53236a8652f494b54a8b67637f8876
kayafox4 <moaikun06@gmail.com>
2025-05-25 03:01:00 +0900

    s

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,10 +3,6 @@ export default {
   content: [
     // 変更点: "./src/..." から "./..." に変更
     "./app/**/*.{js,jsx,mdx}", // app ディレクトリ内のすべてのJS/JSX/MDXファイルを対象
-    // もし components フォルダを app と同じ階層に作成している場合、以下も追加
-    "./components/**/*.{js,jsx,mdx}",
-    // もし pages ディレクトリを使っている場合、以下も追加
-    "./pages/**/*.{js,jsx,mdx}",
   ],
   theme: {
     extend: {
