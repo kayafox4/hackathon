5b6126e2d07e01afdc5a0bae1b685f222c6da2c9
kayafox4 <moaikun06@gmail.com>
2025-05-29 16:48:43 +0900

    s

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,21 +1,21 @@
 // src/app/layout.jsx
 import './globals.css';
 import NavigationBar from './components/NavigationBar'; // NavigationBarをインポート
-import AuthProvider from './providers/NextAuth'; // AuthProviderをインポート (もしあれば)
+import NextAuthProvider from './providers/NextAuth'; // AuthProviderをインポート (もしあれば)
 
 export default function RootLayout({ children }) {
   return (
     <html lang="ja">
       <body>
         {/* NextAuth.jsを使用している場合、AuthProviderで全体を囲む必要があります */}
-        <AuthProvider>
+        <NextAuthProvider>
           {/* メインコンテンツ */}
           <main className="min-h-screen pb-20"> {/* ナビゲーションバーの高さ分だけ下部にパディング */}
             {children}
           </main>
           {/* ナビゲーションバーを最下部に固定表示 */}
           <NavigationBar />
-        </AuthProvider>
+        </NextAuthProvider>
       </body>
     </html>
   );
